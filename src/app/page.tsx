680e0da3b427363bd47b8222728b9f377ba0d882
dimj1k <dimj1k@vk.com>
2024-10-19 12:22:55 +0300

    add to fix guest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {NavigateToResource} from '@refinedev/nextjs-router'
 export default function IndexPage() {
 	return (
 		<Suspense>
-			<Authenticated key="home-page">
+			<Authenticated redirectOnFail={'/login'} key="home-page">
 				<NavigateToResource />
 			</Authenticated>
 		</Suspense>
