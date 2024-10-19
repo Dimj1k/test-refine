e649e12862f8d3118d3efd246a2559be342bda76
dimj1k <dimj1k@vk.com>
2024-10-19 12:20:44 +0300

    fix guest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import {Suspense} from 'react'
 
-import {Authenticated} from '@refinedev/core'
 import {NavigateToResource} from '@refinedev/nextjs-router'
 
 export default function IndexPage() {
